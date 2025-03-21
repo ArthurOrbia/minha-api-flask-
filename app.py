978d405fc22b740f643334ff2ede176ebdd1b1f7
ArthurOrbia <arthur.lima@orbia.ag>
2025-03-21 05:31:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from flask import Flask, jsonify
-... from notion_client import Client
-... 
-... app = Flask(__name__)
-... 
-... # Inicializar o cliente do Notion
-... notion = Client(auth="seu_token_aqui")
-... 
-... @app.route('/')
-... def home():
-...     return "Bem-vindo ao Bot Poe!"
-... 
-... @app.route('/notion-page')
-... def notion_page():
-...     page_id = "seu_page_id_aqui"
-...     page = notion.pages.retrieve(page_id)
-...     return jsonify(page)
-... 
-... if __name__ == '__main__':
+from flask import Flask, jsonify
+from notion_client import Client
+
+app = Flask(__name__)
+
+# Inicializar o cliente do Notion com o token fornecido
+notion = Client(auth="ntn_L9886682044bgWRgx2ZwktT5fd0LLH6sKarImTF9sO1eYu")
+
+@app.route('/')
+def home():
+    return "Bem-vindo ao Bot Poe!"
+
+@app.route('/notion-page')
+def notion_page():
+    # Extrair o page_id do link fornecido
+    page_id = "1bc24d4acbbc8090a3a8e9bf10279463"
+    page = notion.pages.retrieve(page_id)
+    return jsonify(page)
+
+if __name__ == '__main__':
+    app.run(debug=True)
