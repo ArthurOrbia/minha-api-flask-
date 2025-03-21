8b019b448dc4a61ff2138802f7ad6b73310ea3b3
ArthurOrbia <arthur.lima@orbia.ag>
2025-03-21 05:07:42 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from flask import Flask, request, jsonify
+... from notion_client import Client
+... 
+... app = Flask(__name__)
+... 
+... # Inicializar o cliente do Notion
+... notion = Client(auth="ntn_L9886682044bgWRgx2ZwktT5fd0LLH6sKarImTF9sO1eYu")
+... 
+... @app.route('/')
+... def home():
+...     return "Bem-vindo ao Bot Poe!"
+... 
+... @app.route('/notion-page')
+... def notion_page():
+...     page_id = "1bc24d4acbbc8090a3a8e9bf10279463"
+...     page = notion.pages.retrieve(page_id)
+...     return jsonify(page)
+... 
+... if __name__ == '__main__':
